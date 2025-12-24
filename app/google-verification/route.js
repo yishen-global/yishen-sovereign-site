2b81ec69c8292e87611b6a870a751ca8bf454f41
yishen-global <alex.yang@yishenglobal.net>
2025-12-24 08:25:34 +0800

    Delete app/google-verification directory

--- app/google-verification/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return new NextResponse(
-    `google-site-verification: googlexxxx.html`,
-    { headers: { 'Content-Type': 'text/html' } }
-  )
-}
